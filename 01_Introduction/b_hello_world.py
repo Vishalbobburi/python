890c4fe72ca5c009f00221f8b83742a8490d5b8f
Vishalbobburi <bobburivishalchowdary@gmail.com>
2024-12-30 07:15:56 +0000

    class02-Introduction to Python

---
+++ 01_Introduction/b_hello_world.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Purpose: Basic PEP 8 guidelines and 
+       shebang line
+
+
+    PEP - Python Enhancement Prosopal
+    PEP 8 - coding style guide
+
+This is docstring
+
+"""
+
+# print as a statement in python 2
+# print "Hello world!"
+
+# print as a function in python  2 & 3
+print("Hello world!")
+print(True)
+print("True", 123, None)
+
+
+
+
+def wishes(name):
+    wish = "How are you {0}?".format(name)
+    print(wish)
+
+
+wishes("Vishal")
